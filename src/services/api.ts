ea3a10f1e466e7f46aaacaedc9bf51daca04967c
Marcos Henrique <you@example.com>
2023-05-29 23:12:59 -0300

    deploy render

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,7 +7,7 @@ export function setupAPIClient(ctx = undefined){
     let cookies = parseCookies(ctx);
 
     const api = axios.create({
-        baseURL: 'https://backend-pizzaria-o3zn.vercel.app/',
+        baseURL: 'https://api-pizzaria-i745.onrender.com',
         headers: {
             Authorization: `Bearer ${cookies['@nextauth.token']}`
         }
