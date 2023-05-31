93de19840a6180197f0d0e88dafa61e1a32a4862
Marcos Henrique <you@example.com>
2023-05-30 22:41:21 -0300

    link url

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,7 +7,7 @@ export function setupAPIClient(ctx = undefined){
     let cookies = parseCookies(ctx);
 
     const api = axios.create({
-        baseURL: 'https://api-pizzaria-i745.onrender.com',
+        baseURL: 'http://localhost:3333',
         headers: {
             Authorization: `Bearer ${cookies['@nextauth.token']}`
         }
